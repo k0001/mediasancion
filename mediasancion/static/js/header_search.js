7fa4c54cfa97d9ebc6b5384dc127cecb70834751
Renzo Carbonara <gnuk0001@gmail.com>
2011-09-26 19:38:56 -0300

    Search form styling

--- mediasancion/static/js/header_search.js
+++ mediasancion/static/js/header_search.js
@@ -1,9 +1,25 @@
 $(document).ready(function() {
-    $('#search-q').focus(function() {
-        $(this).addClass('highlight').prev('label').hide();
+  if(!Modernizr.input.placeholder) {
+    $('[placeholder]').focus(function() {
+      var input = $(this);
+      if (input.val() == input.attr('placeholder')) {
+        input.val('');
+        input.removeClass('placeholder');
+      }
     }).blur(function() {
-            $(this).removeClass('highlight');
-            if ($(this).val().trim() == "")
-            $(this).val("").prev('label').show();
+      var input = $(this);
+      if (input.val() == '' || input.val() == input.attr('placeholder')) {
+        input.addClass('placeholder');
+        input.val(input.attr('placeholder'));
+      }
+    }).blur();
+    $('[placeholder]').parents('form').submit(function() {
+      $(this).find('[placeholder]').each(function() {
+        var input = $(this);
+        if (input.val() == input.attr('placeholder')) {
+          input.val('');
+        }
+      })
     });
+  }
 });
